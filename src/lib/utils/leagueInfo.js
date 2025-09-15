18dd59eade70e49c5913643e7ec04594cbe838bb
WarrenGSlice <WarrenPeterson141414@Gmail.com>
2025-09-15 05:23:36 -0700

    changed coles favorite team

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -104,7 +104,7 @@ export const managers = [
       "bio": "Cole is does a lot of great things and loves Sam Darnold to the core.",
       "photo": "https://sleepercdn.com/avatars/thumbs/b319fdf8b7b5b0359d3c78622ba4d70c", // square ratio recommended (no larger than 500x500)
       //"fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
-      "favoriteTeam": "det", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+      "favoriteTeam": "sea", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Rebuild", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
         name: "Lou", // Can be anything (usually your rival's name)
