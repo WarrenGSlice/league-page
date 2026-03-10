2f9d24132a2212a9bba88256f631e787f5721cf9
WarrenGSlice <WarrenPeterson141414@Gmail.com>
2026-03-10 15:53:44 -0700

    updated to 2026 season

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "1194782613510815744"; // your league ID
+export const leagueID = "1327557569757061120"; // your league ID
 export const leagueName = "League of Champions"; // your league name
 export const dues = 50; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
