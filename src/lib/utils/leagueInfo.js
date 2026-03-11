87107dcf9116c20605a0132da670c9dee494fed1
WarrenGSlice <WarrenPeterson141414@Gmail.com>
2026-03-10 17:15:53 -0700

    updated nicks fave

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -182,7 +182,7 @@ export const managers = [
         link: 6, // manager array number within this array, or null to link back to all managers page
         image: "https://sleepercdn.com/uploads/68347c2ea943a473b08494f7fe01d370.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
       },
-      "favoritePlayer": 11628, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
+      "favoritePlayer": 11584, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
       "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
       "rookieOrVets": "Rookies", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
       "philosophy": "My Young Players will kick the sh*t out of your old players! ", // (optional)
